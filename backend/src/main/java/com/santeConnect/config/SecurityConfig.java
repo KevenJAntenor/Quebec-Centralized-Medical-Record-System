08ed54faf82169dc9ad56e96b000024930ebf79f
Dominique Elias <elias.dominique@courrier.uqam.ca>
2024-03-16 17:00:21 -0400

    Add Jwt Auth

--- backend/src/main/java/com/santeConnect/config/SecurityConfig.java
+++ backend/src/main/java/com/santeConnect/config/SecurityConfig.java
@@ -1,24 +1,34 @@
 package com.santeConnect.config;
 
+import com.santeConnect.AuthentificationFilter;
 import com.santeConnect.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final UserDetailsServiceImpl userDetailsService;
+    private final AuthentificationFilter authentificationFilter;
 
-    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, AuthentificationFilter authentificationFilter) {
         this.userDetailsService = userDetailsService;
+        this.authentificationFilter = authentificationFilter;
     }
 
     public void configureGlobal (AuthenticationManagerBuilder auth)
@@ -40,5 +50,27 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authentificationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+    return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+                .sessionManagement((sessionManagement) -> sessionManagement.
+                        sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests.requestMatchers(HttpMethod.POST,
+                                "/login")
+                                .permitAll().anyRequest().authenticated())
+                .addFilterBefore(authentificationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
 
 }
---
+++ backend/src/main/java/com/santeConnect/service/JwtService.java
@@ -0,0 +1,55 @@
+package com.santeConnect.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtService {
+    static final long EXPIRATION_DATE = 86400000;
+    // 1 day in ms, TODO change in production.
+    static final String PREFIX = "Bearer";
+
+    // Generate secret key, only for testing
+    // TODO in production, read it only from
+    // app config
+    static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    // Generate signed jwt token
+    public String getToken(String username) {
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_DATE))
+                .signWith(key)
+                .compact();
+
+        return token;
+    }
+
+    // Get a token from request Authorization header,
+    // verify the token, and get username
+    public String getAuthUser(HttpServletRequest request) {
+        String token = request.getHeader
+                (HttpHeaders.AUTHORIZATION);
+
+        if (token != null) {
+            String user = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token.replace(PREFIX, ""))
+                    .getBody()
+                    .getSubject();
+            if (user != null) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+}
