926439435ae079f85d0efe95e3d3439e9ef0c282
Dominique Elias <elias.dominique@courrier.uqam.ca>
2024-03-24 22:20:24 -0400

    Fix post route to add medical visit

--- backend/src/main/java/com/santeConnect/web/MedicalFileController.java
+++ backend/src/main/java/com/santeConnect/web/MedicalFileController.java
@@ -1,12 +1,11 @@
 package com.santeConnect.web;
 
 import com.santeConnect.domain.entities.MedicalFile;
+import com.santeConnect.domain.entities.MedicalVisit;
 import com.santeConnect.repository.entities.MedicalFileRepository;
 
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MedicalFileController {
@@ -26,5 +25,15 @@ public class MedicalFileController {
     public MedicalFile getMedicalFile(@PathVariable Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("MedicalFile not found with id " + id));
+    }
+
+    // post route to add new medical visit to a medical file
+    @PostMapping("medical-files/{id}/medical-visits")
+    public MedicalFile addMedicalVisit(@PathVariable Long id, @RequestBody MedicalVisit medicalVisit) {
+        MedicalFile medicalFile = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("MedicalFile not found with id " + id));
+        medicalVisit.setMedicalFile(medicalFile); // Add this line
+        medicalFile.getMedicalVisitList().add(medicalVisit);
+        return repository.save(medicalFile);
     }
 }
