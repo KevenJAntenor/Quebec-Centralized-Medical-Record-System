b905cd034a84fffdac4149718ebc73cc69bf932d
Dominique Elias <elias.dominique@courrier.uqam.ca>
2024-04-03 16:06:30 -0400

    Add find by patient query

--- backend/src/main/java/com/santeConnect/repository/entities/MedicalFileRepository.java
+++ backend/src/main/java/com/santeConnect/repository/entities/MedicalFileRepository.java
@@ -1,6 +1,8 @@
 package com.santeConnect.repository.entities;
 
 import com.santeConnect.domain.entities.MedicalFile;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -13,6 +15,10 @@ public interface MedicalFileRepository extends CrudRepository<MedicalFile, Long>
 
     // findBy returns List usually, but insuranceNumber is unique so Optional is fine
     Optional<MedicalFile> findByInsuranceNumber(@Param("insuranceNumber") String insuranceNumber);
+
+    // Using query because we are looking inside patient, no medical file
+    @Query("SELECT m FROM MedicalFile m WHERE CONCAT(m.patient.firstName, ' ', m.patient.lastName) LIKE %:fullName%")
+    List<MedicalFile> findByPatientFullName(@Param("fullName") String fullName);
 
     List<MedicalFile> findAll();
     Optional<MedicalFile> findById(Long id);
