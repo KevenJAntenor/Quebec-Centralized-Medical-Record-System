80a9be8232cb1f8699e0d657582c52559c04fe38
Dominique Elias <elias.dominique@courrier.uqam.ca>
2024-04-13 14:27:13 -0400

    Add token to header in main / route

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,15 +1,27 @@
 import type { PageServerLoad } from './$types';
 import { API_URL } from '../constants';
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async ({ locals, request }) => {
     let backendData;
+    const token = locals.token;
     try {
-        const response = await fetch(API_URL);
+        const response = await fetch(API_URL, {
+            method: 'GET',
+            headers: {     
+                'Authorization': `${token}`
+            }
+        });
         backendData = await response.text();
     } catch (error: unknown) {
         backendData = `Make sure your backend is running! Request failed with error: ${error}`;
     }
     return {
-        backendData
+        backendData,
+        message: (token) ? 'you are logged in!' : 'you are not logged in!',
+        props: {
+            locals: {
+                token,
+            },
+        },
     };
 };
